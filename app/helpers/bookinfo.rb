82c8d91af91f80280e81e9f81ad5f5d067c34bea
Toshiyuki Masui <masui@pitecan.com>
2019-03-24 16:39:19 +0900

    Amazon API以外を使うようにしたい

---
+++ app/helpers/bookinfo.rb
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+# -*- ruby -*-
+#
+# Amazonの書籍データを取得
+#
+
+require 'net/http'
+require 'open-uri'
+require 'open_bd'
+require 'json'
+
+class Bookinfo
+  def initialize(isbn)
+    @isbn = isbn
+    @data = {}
+    @data['authors'] = []
+    @data['title'] = ''
+    @data['publisher'] = ''
+    @data['isbn'] = isbn
+
+    amazon
+    openbd unless @data['title']
+    google unless @data['title']
+  end
+  
+  def amazon
+    begin
+      url = "https://www.amazon.co.jp/dp/#{@isbn}"
+      page = open(url)
+      page.read.split(/\n/).each { |line|
+        case
+        when line =~ /<title>/
+          line =~ /<title>([^\|]+)\s+\|.*<\/title>/
+          @data['title'] = $1
+        when line =~ /field-author/
+          line =~ />(.*)<\/a>/
+          @data['authors'] << $1
+        when line =~ /isbn-10/i
+          line =~ /\/b>\s*(.*)<\/li>/
+          @data['isbn'] = $1
+        when line =~ /出版社/
+          line =~ /\/b>\s*([^\(]+)\s*;.*\s*(\(.*\))?<\/li>/
+          @data['publisher'] = $1
+        end
+      }
+    rescue
+    end
+    return @data['title'] != ''
+  end
+
+  def openbd
+    begin
+      client = OpenBD::Client.new
+      res = client.search(isbns: [@isbn])
+    
+      @data['authors'] = []
+      res.body[0]['summary']['author'].split(/\s+/).each { |s|
+        author = ''
+        if s =~ /^(.*)／著/
+          author = $1
+        end
+        if s =~ /^(.*)／翻訳/
+          author = $1
+        end
+        author.gsub!(/,/, ', ')
+        author.gsub!(/([a-z])([A-Z])/,"\\1 \\2")
+        @data['authors'] << author if author != ''
+      }
+      @data['title'] = res.body[0]['summary']['title']
+      @data['publisher'] = res.body[0]['summary']['publisher']
+    rescue
+    end
+  end
+  
+  def google
+    begin
+      uri = URI.parse("https://www.googleapis.com/books/v1/volumes?q=isbn:#{@isbn}")
+      data = JSON.parse uri.read
+      volumeinfo = data['items'][0]['volumeInfo']
+      @data['publisher'] = ''
+      @data['title'] = volumeinfo['title']
+      @data['authors'] = volumeinfo['authors']
+    rescue
+    end
+  end
+  
+  def image
+    cands = [
+      ["images-jp.amazon.com", "/images/P/#{@isbn}.01._OU09_PE0_SCMZZZZZZZ_.jpg"], # 2006/3/6 tsuika
+      ["images-jp.amazon.com", "/images/P/#{@isbn}.09.MZZZZZZZ.jpg"],
+      ["images-jp.amazon.com", "/images/P/#{@isbn}.01.MZZZZZZZ.jpg"],
+      ["bookweb.kinokuniya.co.jp","/imgdata/#{@isbn}.jpg"],
+    ]
+    imageurl(cands)
+  end
+
+  def imageurl(cands)
+    ret = ""
+    cands.each { |c|
+      host = c[0]
+      path = c[1]
+      url = "http://#{host}#{path}"
+      response = ''
+      Net::HTTP.start(host, 80) { |http|
+        response = http.get(path)
+      }
+      if response.class == Net::HTTPOK && response.body.length > 1000 then
+        ret = url
+        break
+      end
+    }
+    ret
+  end
+
+  def title
+    @data['title']
+  end
+  
+  def publisher
+    @data['publisher']
+  end
+  
+  def authors
+    @data['authors']
+  end
+  
+end
+
+if __FILE__ == $0 then
+  isbn = '0262011530'
+  isbn = '4063192393'
+  isbn = '4065020352'
+  bookinfo = Bookinfo.new(isbn)
+  puts bookinfo.title
+  puts bookinfo.publisher
+  puts bookinfo.authors.join(', ')
+  
+  #puts bookinfo.publisher(isbn)
+  #puts bookinfo.authors(isbn)
+  #puts bookinfo.url(isbn)
+  puts bookinfo.image
+end
